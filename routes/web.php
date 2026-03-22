fab62c11fe1a4cfe42690e90f2faf1013fa38cf1
ZahidKhan1100 <kzahid416@gmail.com>
2026-03-22 13:59:52 +0000

    version updated

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
+
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test-mail', function () {
+    try {
+        Mail::raw('This is a test email from Mailgun', function ($message) {
+            $message->to('kzahid416@gmail.com')
+                    ->subject('Mailgun Test');
+        });
+
+        return '✅ Test email sent successfully!';
+    } catch (\Exception $e) {
+        return '❌ Mail sending failed: ' . $e->getMessage();
+    }
 });
