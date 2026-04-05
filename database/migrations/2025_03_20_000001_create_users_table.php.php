941f0a5f95904408e271537c045bece2c567a210
ZahidKhan1100 <kzahid416@gmail.com>
2026-04-05 19:15:03 +0100

    complete version 1.0

--- database/migrations/2025_03_20_000001_create_users_table.php.php
+++ database/migrations/2025_03_20_000001_create_users_table.php.php
@@ -14,7 +14,7 @@ return new class extends Migration {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('password')->nullable();
 
             $table->unsignedBigInteger('house_id')->nullable(); // no FK yet
             $table->unsignedBigInteger('trip_id')->nullable();
